0172af06899d797f9b799891901d3672d5a11004
ignacio <ignacio.tumini@hotmail.com>
2023-09-13 15:41:04 -0300

    update logger

---
+++ src/utils/logs/logger.js
@@ -0,0 +1,69 @@
+import winston from "winston";
+
+const levelOptions = {
+  levels: {
+    fatal: 0,
+    error: 1,
+    warning: 2,
+    info: 3,
+    htto: 4,
+    debug: 5,
+  },
+
+  colors: {
+    fatal: "red",
+    error: "orange",
+    warning: "yellow",
+    info: "blue",
+    http: "green",
+    debug: "white",
+  },
+};
+
+const logger = winston.createLogger({
+  levels: levelOptions.levels,
+  transports: [
+    new winston.transports.Console({
+      level: "info",
+      format: winston.format.combine(
+        winston.format.colorize({ colors: levelOptions.colors }),
+        winston.format.simple()
+      ),
+    }),
+
+    new winston.transports.File({
+      filename: "./errors.log",
+      level: "warning",
+      format: winston.format.simple(),
+    }),
+    //new winston.transports.File({ filename:'./errores.log', level:'warn'})
+  ],
+});
+export const addLogger = (req, res, next) => {
+  req.logger = logger;
+  req.logger.info(
+    `${req.method} en ${req.url} -${new Date().toLocaleTimeString()}`
+  );
+  req.logger.warning(
+    `${req.method} en ${req.url} -${new Date().toLocaleTimeString()}`
+  );
+  //req.logger.console.warn();(`${req.method} en ${req.url} -${new Date().toLocaleTimeString()}`)
+  next();
+};
+
+/*
+
+// asi se importaria en app, pero es muy robusto
+
+app.use(addLogger)
+
+ 
+
+app.get('/',(req,res)=>{
+
+    req.logger.warning("Tenemos una alerta");
+
+    res.send({message: "Estamos probando Logger con Winston"})
+
+})
+*/
